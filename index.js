9ceed25de6fc10724e3410698451378538f6ce91
Viclor27322 <vito27322@gamil.com>
2023-03-09 08:16:42 -0600

    add files

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose= require('mongoose');
+require('dotenv').config();
+const productoRoutes = require('./src/routes/producto');
+
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("Welcome to my API")
+})
+
+//midelware
+app.use(express.json());
+app.use('/api',productoRoutes)
+
+
+///conexion
+mongoose.connect(
+    process.env.MONGODB_URI
+).then(()=>console.log('Conected to MongoDB'))
+.catch((error)=>console.error(error));
+
+
+app.listen(port,()=>console.log('Server listen on port ', port));
+
+
+//se intala, exprese, nodemon, mongoose, dotenv
