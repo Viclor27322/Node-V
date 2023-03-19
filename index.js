89fc854ea797a5a18765621bf93c9dac335cd78b
Viclor27322 <vito27322@gamil.com>
2023-03-19 17:32:47 -0600

    update files

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.use('',UserRoutes);
 app.use(cors({
     origin: '*'
   }));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+  
+  
+  
+  
 
 
 ///conexion
