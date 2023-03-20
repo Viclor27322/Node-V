0151487b7e75d5d5844743fe32389bac179ac7a4
Viclor27322 <vito27322@gamil.com>
2023-03-20 13:28:48 -0600

    update files

--- index.js
+++ index.js
@@ -17,7 +17,17 @@ app.get('/',(req,res)=>{
 
 //midelware
 app.use(express.json());
-
+app.use(cors());
+app.use(cors({
+    origin: '*'
+  }));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 app.use('/api',productoRoutes);
 app.use('/api',productosRoutes);
 app.use('/api',TerrRoutes);
