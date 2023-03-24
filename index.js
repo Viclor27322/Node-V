86f208fa2e8ebb3e71daa0d3eb6d875efb6445e3
Viclor27322 <vito27322@gamil.com>
2023-03-23 19:16:41 -0600

    update files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const productoRoutes = require('./src/routes/producto');
 const productosRoutes = require('./src/routes/productos');
 const TerrRoutes = require('./src/routes/terrario');
 const UserRoutes = require('./src/routes/users');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
@@ -16,7 +17,8 @@ app.get('/',(req,res)=>{
 })
 
 //midelware
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -24,7 +26,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
-app.use('/public',express.static(`${__dirname}/img`))
+app.use('/public',express.static(`${__dirname}/src/img`))
 app.use('/api',productoRoutes);
 app.use('/api',productosRoutes);
 app.use('/api',TerrRoutes);
--- src/storage/storage.js
+++ src/storage/storage.js
@@ -2,10 +2,10 @@ const multer =require ('multer');
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,'./img');
+        cb(null,'../img');
     },
     filename: function(req,file,cb){
-        cb(null,`${file.fieldname}-${Date.now()}`)
+        cb(null,`${file.fieldname}-${Date.now()}.png`)
     }
 });
 
