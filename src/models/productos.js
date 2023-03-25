9aaea3f70b5246bdce828e77a068a99b6842dec8
Viclor27322 <126909284+Viclor27322@users.noreply.github.com>
2023-03-24 21:22:13 -0600

    Update productos.js

--- src/models/productos.js
+++ src/models/productos.js
@@ -2,34 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const ProdSchema = new Schema({
-    nombre:{
-        type: String,
-        require:true
-    },
-    descripcion:{
-        type:String,
-        require:true
-    },
-    imagen:{
-        public_id:String,
-        secure_url:String
-    },
-    precio:{
-        type:Number,
-        require:true
-    },
-    sabor:{
-        type:String,
-        require:true
-    },
-    presentacion:{
-        type:String,
-        require:true
-    },
-    existencia:{
-        type:Number,
-        require:true
-    }
+  nombre: {
+    type: String,
+    require: true,
+  },
+  descripcion: {
+    type: String,
+    require: true,
+  },
+  imagen: {
+    public_id: String,
+    secure_url: String,
+  },
+  precio: {
+    type: Number,
+    require: true,
+  },
+  sabor: {
+    type: String,
+    require: true,
+  },
+  presentacion: {
+    type: String,
+    require: true,
+  },
+  existencia: {
+    type: Number,
+    require: true,
+  },
+  categoria: {
+    type: String,
+    require: true,
+  },
 });
+
 
 module.exports = mongoose.model('Productos',ProdSchema);
