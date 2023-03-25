bf7aaafd84d08b323a08d994dc3ed1abe8743c26
Viclor27322 <vito27322@gamil.com>
2023-03-24 18:22:27 -0600

    update files

--- src/cloudinary/cloudinary.js
+++ src/cloudinary/cloudinary.js
@@ -11,7 +11,7 @@ cloudinary.config({
 
 export async function uploadImage(filePath){
     return await cloudinary.uploader.upload(filePath,{
-        folder:'replit'
+        folder:'proyecto'
     })
 }
 
