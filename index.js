3b908ecd37254ebf858156499d0f5e02b4da80c1
Viclor27322 <vito27322@gamil.com>
2023-03-24 19:07:31 -0600

    update files

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const productoRoutes = require('./src/routes/producto');
 const productosRoutes = require('./src/routes/productos');
 const TerrRoutes = require('./src/routes/terrario');
 const UserRoutes = require('./src/routes/users');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
@@ -19,14 +18,13 @@ app.get('/',(req,res)=>{
 //midelware
 
 app.use(cors());
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+  }); */
 app.use(express.json());
-app.use('/public',express.static(`${__dirname}/src/img`))
 app.use('/api',productoRoutes);
 app.use('/api',productosRoutes);
 app.use('/api',TerrRoutes);
