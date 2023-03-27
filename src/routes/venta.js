9d15a44feb859a2a9f0a35c506b9082f9530a682
Viclor27322 <vito27322@gamil.com>
2023-03-26 20:23:20 -0600

    update files

--- src/routes/venta.js
+++ src/routes/venta.js
@@ -5,10 +5,22 @@ const VentaSchema = require('../models/venta');
 //crear
 router.post('/venta'),(req,res)=>{
     const {usuario,productos,cantidad,total}=req.body;
+    let fechaActualD = new Date();
+    let dia = fechaActualD.getDate().toString().padStart(2, "0"); // Agrega un cero a la izquierda si el día tiene un solo dígito
+    let mes = (fechaActualD.getMonth() + 1).toString().padStart(2, "0"); // Agrega un cero a la izquierda si el mes tiene un solo dígito
+    let anio = fechaActualD.getFullYear().toString().slice(-2); // Toma los últimos dos dígitos del año
 
+    let fechaFormateada = dia + "-" + mes + "-" + anio;
+
+    let fechaActual = new Date();
+    let hora = fechaActual.getHours().toString().padStart(2, "0"); // Agrega un cero a la izquierda si la hora tiene un solo dígito
+    let minutos = fechaActual.getMinutes().toString().padStart(2, "0"); // Agrega un cero a la izquierda si los minutos tienen un solo dígito   
+
+    
+    let horaFormateada = hora + ":" + minutos;
     const venta = new VentaSchema({
-        fecha : "0-0-0",
-        hora : "0-0-0",
+        fecha : fechaFormateada,
+        hora : horaFormateada,
         usuario, 
         productos:[ 
             productos,
