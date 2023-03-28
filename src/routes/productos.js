a69ce037067608a1949a194f559a6ce1e0fb8aa0
Viclor27322 <vito27322@gamil.com>
2023-03-27 22:14:39 -0600

    update files

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const ProdSchema= require('../models/productos');
-const uploadImage = require('../cloudinary/cloudinary');
 
 
 //crear
